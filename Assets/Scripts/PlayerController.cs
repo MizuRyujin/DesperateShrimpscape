782f7ecdfb8601403f157ff91682f976052848bf
Miguel Fernández <miguelrf95@gmail.com>
2025-03-29 11:30:25 +0000

    Create player states enmu and player controller

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,6 +2,17 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private PlayerStates _currentState;
+
+    /// <summary>
+    /// Awake is called when the script instance is being loaded.
+    /// </summary>
+    private void Awake()
+    {
+        _currentState = PlayerStates.PAUSED;
+        Debug.Log(_currentState);
+    }
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
