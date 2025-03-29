d5b7db5f82b6a367e53b74a3245ae4647b994f66
Iv-Abr <ivanabramov789@gmail.com>
2025-03-29 21:05:29 +0000

    Snd_upd

--- Assets/Scripts/PlayerSounds.cs
+++ Assets/Scripts/PlayerSounds.cs
@@ -5,6 +5,7 @@ using FMOD.Studio;
 public class PlayerSounds : MonoBehaviour
 {
     [SerializeField] EventReference _moveSound;
+    [SerializeField] EventReference _gunSound;
     EventInstance _event;
 
     public void MoveNoise(int noiseLevel)
@@ -13,6 +14,14 @@ public class PlayerSounds : MonoBehaviour
         RuntimeManager.AttachInstanceToGameObject(_event, gameObject);
         Debug.Log("Noise Lvl:" + noiseLevel);
         _event.setParameterByName("Noise_Level", noiseLevel);
+        _event.start();
+
+
+    }
+    public void GunSound()
+    {
+        _event = RuntimeManager.CreateInstance(_gunSound);
+        RuntimeManager.AttachInstanceToGameObject(_event, gameObject);
         _event.start();
     }
 }
